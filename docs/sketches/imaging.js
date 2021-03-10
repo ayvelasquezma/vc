0753f7e7e3a95f8957517c0db07444b39a908ca6
ayvelasquezma <61332233+ayvelasquezma@users.noreply.github.com>
2021-03-10 15:52:21 +0000

    Deploying to gh-pages from  @ 77a9df8838d18dd866276ab79424e2e94044d7e4 🚀

---
+++ docs/sketches/imaging.js
@@ -0,0 +1,15 @@
+  
+let img;
+
+var myp5 = new p5((p) => {
+    p.setup = function () {
+        // p.createCanvas(600, 600);
+        img = p.createImg("/vc/docs/sketches/lenna.png")
+        p.noLoop();
+    }
+
+    p.draw = function () {
+        p.image(img,0,0);
+    }
+
+}, "image");
